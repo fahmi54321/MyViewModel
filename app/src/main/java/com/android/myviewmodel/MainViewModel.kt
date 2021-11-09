186db4d9d993733128f7a930fca0c3ad3c0f5dfa
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-11-09 20:07:54 +0700

    Initial commit

---
+++ app/src/main/java/com/android/myviewmodel/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.android.myviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel:ViewModel() {
+    var result = 0
+
+    fun calculate(
+        width: String,
+        height: String,
+        length: String
+    ){
+        result = width.toInt()*height.toInt()*length.toInt()
+    }
+}
